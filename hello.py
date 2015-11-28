836e37fc63e5a99e2b3db9cf501a4937659d14c9
Vadim <detriment.ehl@gmail.com>
2015-11-28 11:52:38 -0500

    Added mainframe row configuration.

--- hello.py
+++ hello.py
@@ -16,5 +16,6 @@ root.title("Feet To Meters")
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
 
 root.mainloop()
