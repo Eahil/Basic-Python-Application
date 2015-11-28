ef72a078b04f3f3ede0202be4a0b36d38ef9e2ad
Vadim <detriment.ehl@gmail.com>
2015-11-28 12:05:11 -0500

    Added button and field for typing.

--- hello.py
+++ hello.py
@@ -24,6 +24,9 @@ meters = StringVar()
 feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
 feet_entry.grid(column=2, row=1, sticky=(W, E))
 
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
 feet_entry.focus()
