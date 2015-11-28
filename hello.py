5fa743c27af30565a0897f1b9ecc30a363144ace
Vadim <detriment.ehl@gmail.com>
2015-11-28 09:30:44 -0500

    Added variables and func calls to calc try.

--- hello.py
+++ hello.py
@@ -5,7 +5,8 @@ from tkinter import ttk
 
 def calculate(*args):
     try:
-        pass
+        value = float(feet.get())
+        meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
     except ValueError:
         pass
 
